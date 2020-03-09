b15432bb94bd018b1295efa6159e9ec84c4ac1e7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-09 22:49:35 +0000

    Deploy dev-launchers-sandbox/project__pong-adventure to github.com/dev-launchers-sandbox/project__pong-adventure.git:gh-pages

---
+++ classes/Door.js
@@ -0,0 +1,18 @@
+import Phaser from "phaser";
+
+export default class Door extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, x, y) {
+    super(scene, x, y, scene.generateRectangleSprite(30, 40));
+    this.scene = scene;
+
+    // Add to rendering engine
+    scene.add.existing(this);
+    // Add to physics engine
+    scene.physics.add.existing(this);
+  }
+  update() {}
+
+  destroy() {
+    super.destroy();
+  }
+}
